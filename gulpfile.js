ba54f6e58c3c4211e3c92b199f719dc10a2c6fe0
Raven737 <prvm79@gmail.com>
2022-08-13 20:19:20 +0300

    0007

--- gulpfile.js
+++ gulpfile.js
@@ -3,68 +3,74 @@ import del from 'del';
 import gulp from 'gulp';
 import cssmin from 'gulp-cssmin';
 import fileinclude from 'gulp-file-include';
-import imagemin from 'gulp-imagemin';
+import htmlmin from 'gulp-htmlmin';
+// import imagemin from 'gulp-imagemin';
 import rename from 'gulp-rename';
+import size from 'gulp-size';
 import uglify from 'gulp-uglify';
 import dartSass from 'sass';
 import gulpSass from 'gulp-sass';
 const sass = gulpSass(dartSass);
 
 export const deleteDist = () => {
-    return del('dist');
+    return del('./dist');
 };
 
 export const html = () => {
-    return gulp.src(['#src/*.html', '!#src/_*.html'])
+    return gulp.src(['./#src/html/**/*.html', '!./#src/html/**/_*.html'])
         .pipe(fileinclude())
-        .pipe(gulp.dest('dist'))
+        .pipe(gulp.dest('./dist/html (origin Size)'))
+        .pipe(size({ title: "До стискання: " }))
+        .pipe(htmlmin({ collapseWhitespace: true }))
+        .pipe(size({ title: "Після стискання: " }))
+        .pipe(gulp.dest('./dist'))
         .pipe(browsersync.stream())
 };
 
 export const css = () => {
-    return gulp.src('#src/scss/**/*.scss')
+    return gulp.src('./#src/scss/**/*.scss')
         .pipe(sass().on('error', sass.logError))
-        .pipe(gulp.dest('dist/css'))
+        .pipe(gulp.dest('./dist/css'))
         .pipe(cssmin())
-        .pipe(rename({ extname: ".min.css" }))
-        .pipe(gulp.dest('dist/css'))
+        .pipe(rename({ suffix: ".min" }))
+        .pipe(gulp.dest('./dist/css'))
         .pipe(browsersync.stream())
 };
 
 export const js = () => {
-    return gulp.src('#src/js/**/*.js')
-        .pipe(gulp.dest('dist/js'))
+    return gulp.src('./#src/js/**/*.js')
+        .pipe(gulp.dest('./dist/js'))
         .pipe(uglify())
-        .pipe(rename({ extname: ".min.js" }))
-        .pipe(gulp.dest('dist/js'))
+        .pipe(rename({ suffix: ".min" }))
+        .pipe(gulp.dest('./dist/js'))
         .pipe(browsersync.stream())
 }
 
 export const images = () => {
-    return gulp.src('#src/images/**/*')
-        .pipe(imagemin())
-        .pipe(gulp.dest('dist/images'))
+    return gulp.src('./#src/images/**/*')
+        // .pipe(imagemin())
+        .pipe(gulp.dest('./dist/images'))
         .pipe(browsersync.stream())
 }
 
 export const fonts = () => {
-    return gulp.src('#src/fonts/**/*')
-        .pipe(gulp.dest('dist/fonts'))
+    return gulp.src('./#src/fonts/**/*')
+        .pipe(gulp.dest('./dist/fonts'))
         .pipe(browsersync.stream())
 }
 
 export const watch = () => {
-    gulp.watch(['#src/*.html'], build);
-    gulp.watch(['#src/scss/**/*.scss'], build);
-    gulp.watch(['#src/js/**/*.js'], build);
-    gulp.watch(['#src/images/**/*'], build);
-    gulp.watch(['#src/fonts/**/*'], build);
+    gulp.watch(['./#src/html/**/*.html'], creation);
+    gulp.watch(['./#src/scss/**/*.scss'], creation);
+    gulp.watch(['./#src/js/**/*.js'], creation);
+    gulp.watch(['./#src/images/**/*'], creation);
+    gulp.watch(['./#src/fonts/**/*'], creation);
 };
 
 export const sync = () => {
     browsersync.init({
         server: {
-            baseDir: "dist"
+            baseDir: "./dist"
         },
         port: 3000,
         notify: true
